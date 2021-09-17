4c73806d0ecff6162513270ed2ff0c7410327162
Mikkel Andreasen <38246850+TheProphet4000@users.noreply.github.com>
2021-09-17 10:53:53 +0200

    Create ChatClientAflevering.java

---
+++ src/ChatClientAflevering.java
@@ -0,0 +1,10 @@
+public class ChatClientAflevering {
+    /*
+    Beskriv dens protokol med et par linjer i Readme.md, og programmer en command line chatclient, som kan bruges sammen med serveren. Skriv fx:
+            "Serveren lytter på port x"
+            "Serveren åbner en inputstream og outsputstream for hver client"
+            "Kommandoen SUBMITNAME sende fra ... til ... og det betyder.. "
+     */
+
+
+}
