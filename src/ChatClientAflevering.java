c15baf1c34c6e1f0425f28e609dbbb44bdaab557
Mikkel Andreasen <38246850+TheProphet4000@users.noreply.github.com>
2021-09-17 14:39:07 +0200

    Update ChatClientAflevering.java
    Exception in thread "main" java.net.ConnectException: Connection refused: connect

--- src/ChatClientAflevering.java
+++ src/ChatClientAflevering.java
@@ -3,6 +3,7 @@ programmer en command line chatclient, som kan bruges sammen med serveren.
  */
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.Objects;
 import java.util.Scanner;
@@ -21,13 +22,41 @@ public class ChatClientAflevering {
         System.out.println("Hvis du kun ønsker at skrive lokalt, tryk på Enter tasten");
 
         Scanner scan = new Scanner(System.in); ipAdress = scan.nextLine();
-        if (Objects.equals(ipAdress, "")){ ipAdress = "localhost";}
+        if (Objects.equals(ipAdress, "")){ ipAdress = "localhost"; System.out.println("opretter localhost forbindelse"); }
 
-        client();
+        clientSetup();
     }
 
-    static void client() throws IOException {
+    static void clientSetup() throws IOException {
         var soc = new Socket(ipAdress,port);
 
+        Scanner inp = new Scanner(soc.getInputStream());
+        PrintWriter out = new PrintWriter(soc.getOutputStream(),true);
+
+        while (inp.hasNextLine()){
+            var line = inp.nextLine();
+
+            if (line.startsWith("SUBMITNAME")) {
+                System.out.println("Skriv dit navn"); String mitNavn = inp.nextLine();
+
+                out.println(mitNavn);
+
+            } else if (line.startsWith("NAMEACCEPTED")) {
+                System.out.println("Chatter - " + line.substring(13));
+
+                Scanner scanner = new Scanner(System.in);
+                System.out.println("Skriv hvad du har på hjertet"); String besked = scanner.nextLine();
+
+                out.println(besked);
+
+            } else if (line.startsWith("MESSAGE")) {
+                System.out.println(line.substring(8) + "\n");
+
+                Scanner scanner = new Scanner(System.in);
+                System.out.println("Skriv hvad du har på hjertet"); String besked = scanner.nextLine();
+
+                out.println(besked);
+            }
+        }
     }
 }
